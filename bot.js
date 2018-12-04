f90a250491c86cb0b97d65e60ea16dd0cfc6522e
w2lody <44412859+w2lody@users.noreply.github.com>
2018-12-04 22:13:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,12 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("517101519628402702");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**:fallen_leaf: __#-Bienvenue à ~ Venom__ :black_heart: :comet: . **`), 4000)        
+}
 });
 
-
-client.on('ready', async() => {
-var server = "476460414629183490"; // ايدي السررفر
-var channel = "513323005481320459";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo , ')
-    },305);
-})
-
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
